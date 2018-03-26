66a7ebfdbe6d2dc0aa1177354ccdb32fe7855923
Heike Hofmann <hofmann@iastate.edu>
2018-03-26 18:00:21 -0500

    mini example added for write_x3p

--- R/write_x3p.R
+++ R/write_x3p.R
@@ -8,10 +8,8 @@
 #' @export
 #' @examples
 #' logo <- read_x3p(system.file("csafe-logo.x3p", package="x3ptools"))
-#' \dontrun{
-#' # write copy of the file in the current working directory
-#' write_x3p(logo, "logo.x3p") 
-#' }
+#' # write a copy of the file into a temporary file
+#' write_x3p(logo, file = tempfile(fileext="x3p")) 
 write_x3p <- function(x3p, file)
 {
   a1 <- read_xml(system.file("templateXML.xml", package="x3ptools"))
