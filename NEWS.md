9a5cb321a146045a4eb91213ea468b89896f611a
Heike Hofmann <hofmann@iastate.edu>
2018-06-21 16:01:17 -0500

    update to allow for offset sampling

--- NEWS.md
+++ NEWS.md
@@ -1,1 +1,6 @@
 # x3ptools 0.0.1.9000
+
+## features
+- sample_x3p: added possibility to sample at different rates in x and y direction, added offsets in sampling
+- print.x3p for showing a concise summary of relevant meta information from the xml file. 
+## bug fixes
--- R/sample_x3p.R
+++ R/sample_x3p.R
@@ -1,7 +1,10 @@
 #' Sample from an x3p object
 #' 
 #' @param x3p x3p object
-#' @param m integer value - every mth value is included in the sample
+#' @param m integer value - every mth value is included in the sample 
+#' @param mY integer value - every mth value is included in the sample in x direction and every mYth value is included in y direction 
+#' @param offset integer value between 0 and m-1 to specify offset of the sample
+#' @param offsetY integer value between 0 and mY-1 to specify different offsets for x and y direction
 #' @return down-sampled x3p object
 #' @export
 #' @examples
@@ -14,11 +17,11 @@
 #' image_x3p(logo)
 #' image_x3p(logo4)
 #' }
-sample_x3p <- function(x3p, m=2) {
+sample_x3p <- function(x3p, m=2, mY=m, offset = 0, offsetY = offset) {
   stopifnot("x3p" %in% class(x3p))
   sizes <- dim(x3p$surface.matrix)
-  seqx <- seq.int(from = 1, to = sizes[1], by=m)
-  seqy <- seq.int(from = 1, to = sizes[2], by=m)
+  seqx <- seq.int(from = 1+offset, to = sizes[1], by=m)
+  seqy <- seq.int(from = 1+offsetY, to = sizes[2], by=mY)
   
   
   x3p$surface.matrix <- x3p$surface.matrix[seqx, seqy]
