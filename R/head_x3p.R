b5ee58218078e50b3cc20c0b0ebe1e33ea63b9c6
Susan Vanderplas <srvanderplas@gmail.com>
2018-07-09 21:26:05 -0500

    head function

---
+++ R/head_x3p.R
@@ -0,0 +1,21 @@
+#' Show meta information of an x3p file
+#' 
+#' `head.x3p` expands the generic head method for x3p objects. It gives a summary of the most relevant 3p meta information and returns the object invisibly.
+#' @param x x3p object
+#' @param n number of rows/columns of the matrix
+#' @param ... extra parameters passed to head.matrix()
+#' @importFrom utils head
+#' @importFrom utils head.matrix
+#' @export
+#' @examples 
+#' logo <- read_x3p(system.file("csafe-logo.x3p", package="x3ptools"))
+#' head(logo)
+#' @method head x3p
+head.x3p <- function (x, n = 6L, ...) 
+{
+  cat(sprintf("size (width x height): %d x %d in pixel \n", 
+              x$header.info$sizeX, x$header.info$sizeY))
+  cat(sprintf("resolution (um x um): %.4f x %.4f \n", x$header.info$incrementX * 
+                10^6, x$header.info$incrementY * 10^6))
+  head.matrix(x$surface.matrix[,1:n], n = n, ...)
+}
