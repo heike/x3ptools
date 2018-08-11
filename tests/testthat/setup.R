bcb6d5218d806fffc63fe1b52fb024ece6cbb8e6
Susan VanderPlas (root) <srvanderplas@gmail.com>
2018-08-10 20:48:19 -0500

    fix setup to not refer to here package until it's needed

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,8 +1,8 @@
 # Setup file for tests
 
 # List files that are present before the start of testing
-if (!exists("okfiles")) {
-  okfiles <- list.files(here::here("tests/"), ".Rdata", full.names = T)
-}
+# if (!exists("okfiles")) {
+#   okfiles <- list.files(here::here("tests/"), ".Rdata", full.names = T)
+# }
 
 `%>%` <- magrittr::`%>%`
