884c4a0b46d18cf6ef0bd214f35b09f69d2c6dbf
Susan Vanderplas <srvanderplas@gmail.com>
2018-08-13 16:46:46 -0500

    test issues

--- tests/testthat/test_download_NIST.R
+++ tests/testthat/test_download_NIST.R
@@ -22,7 +22,7 @@ test_that("download_NIST works as expected", {
   expect_equal(gsub("^(.*)\\.", "", list.files("downloadNist/Hi-Point 01/HiPoint 1-2/Hamby Hi-Point C9 Sl/", 
                                                recursive = T, include.dirs = F)),
                c("png", "png", "png", "x3p", "xlsx"))
-  expect_equal(length(list.files("downloadNist", "*.x3p", recursive = T)), 2)
+  expect_equal(length(list.files("downloadNist", "*.x3p", recursive = T)), maxfiles)
   unlink("downloadNist/*", recursive = T)
   
   # File Structure Mirroring is false
@@ -31,8 +31,21 @@ test_that("download_NIST works as expected", {
   expect_equal(list.files("downloadNist", recursive = F, include.dirs = T), "Hamby Hi-Point C9 Sl")
   expect_equal(gsub("^(.*)\\.", "", list.files("downloadNist/", recursive = T, include.dirs = F)),
                c("png", "png", "png", "x3p", "png", "png", "png", "x3p", "xlsx"))
-  expect_equal(length(list.files("downloadNist", "*.x3p", recursive = T)), 2)
+  expect_equal(length(list.files("downloadNist", "*.x3p", recursive = T)), maxfiles)
   unlink("downloadNist/*", recursive = T)
+  
+  # full download link
+  # expect_silent(NRBTD_download("https://tsapps.nist.gov/NRBTD/Studies/Studies/Details/4908a64a-702c-4203-a945-6279df3acf3f", 
+  #                              directory = "downloadNist", 
+  #                                mirrorFileStructure = F, maxFiles = maxfiles))
+  # expect_equal(list.files("downloadNist", recursive = F, include.dirs = T), "Hamby Hi-Point C9 Sl")
+  # expect_equal(gsub("^(.*)\\.", "", list.files("downloadNist/", recursive = T, include.dirs = F)),
+  #              c("png", "png", "png", "x3p", "png", "png", "png", "x3p", "xlsx"))
+  # expect_equal(length(list.files("downloadNist", "*.x3p", recursive = T)), maxfiles)
+  # unlink("downloadNist/*", recursive = T)
+  
+  # Page that doesn't exist
+  expect_error(NRBTD_download("4908a64a-702c-4203-a945-62734f3acf3f", "downloadNist", maxFiles = maxfiles, quiet = T))
 })
 
 test_that("NRBTDsample_download works as expected", {
