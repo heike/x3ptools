0fe5ead0e9f6a81e9f284113be7f9c22e7a45f5f
Susan Vanderplas <srvanderplas@gmail.com>
2018-08-13 21:26:54 -0500

    x3p_to_df test

--- tests/testthat/test_df_to_x3p.R
+++ tests/testthat/test_df_to_x3p.R
@@ -5,7 +5,7 @@ dftest <- expand.grid(x = 1:6, y = 1:6) %>%
   as.data.frame() %>%
   dplyr::mutate(z = rnorm(36, 0, .1) + sqrt((x - 3.5)^2 + (y - 3.5)^2),
                 value = z)
-
+x3ptest <- df_to_x3p(dftest[,c(1, 2, 4)])
 # Test missing values messages
 # dftest2 <- dftest[sample(1:36, size = 34),]
 
@@ -20,4 +20,12 @@ test_that("df_to_x3p works as expected", {
                     matrix(dplyr::arrange(dftest, desc(y))$value,
                            byrow = F, nrow = 6))
   expect_equivalent(tmp$header.info, list(sizeX = 6, sizeY = 6, incrementX = 1, incrementY = 1))
+})
+
+test_that("x3p_to_df works as expected", {
+  expect_silent(tmp <- x3p_to_df(x3ptest))
+  expect_equivalent(tmp %>% 
+                      dplyr::mutate(x = x + 1, y = y + 1) %>% 
+                      dplyr::arrange(y, x), 
+                    dftest[,c(1, 2, 4)])
 })
