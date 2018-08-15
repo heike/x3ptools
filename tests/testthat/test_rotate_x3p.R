b90769580b7b2e8423dd361e119b9546f6a9d29b
Susan Vanderplas <srvanderplas@gmail.com>
2018-08-14 20:51:01 -0500

    test rotate_x3p files

--- tests/testthat/test_rotate_x3p.R
+++ tests/testthat/test_rotate_x3p.R
@@ -4,6 +4,8 @@ rotate_surface_mat_90 <- function(x) {
   t(x)[,c(rev(1:nrow(x)))]
 }
 x3ptest90 <- rotate_x3p(x3ptest)
+x3ptest_transpose <- transpose_x3p(x3ptest)
+x3ptest_yflip <- y_flip_x3p(x3ptest)
 
 test_that("rotate_x3p works as expected", {
   expect_equivalent(rotate_x3p(x3ptest, angle = 0), x3ptest)
@@ -14,4 +16,14 @@ test_that("rotate_x3p works as expected", {
   expect_equivalent(x3ptest90$header.info$sizeY, x3ptest$header.info$sizeX)
   expect_equivalent(x3ptest90$surface.matrix, 
                     rotate_surface_mat_90(x3ptest$surface.matrix))
+})
+
+test_that("transpose_x3p works as expected", {
+  expect_equivalent(x3ptest$surface.matrix, t(x3ptest_transpose$surface.matrix))
+  expect_equivalent(x3ptest$header.info$sizeX, x3ptest_transpose$header.info$sizeY)
+  expect_equivalent(x3ptest$header.info$sizeY, x3ptest_transpose$header.info$sizeX)
+})
+
+test_that("y_flip_x3p works as expected", {
+  expect_equivalent(x3ptest_yflip$surface.matrix, x3ptest$surface.matrix[,rev(1:ncol(x3ptest$surface.matrix))])
 })
