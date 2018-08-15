328573323b30fce6977e793f03db8c29108c5cd6
Susan Vanderplas <srvanderplas+github@gmail.com>
2018-08-15 07:38:22 -0500

    tests for test_sample_x3p

--- tests/testthat/test_sample_x3p.R
+++ tests/testthat/test_sample_x3p.R
@@ -1,8 +1,17 @@
 context("sample_x3p")
 
 x3ptest12 <- sample_x3p(x3ptest, 1, 2)
+x3ptest12.1 <- sample_x3p(x3ptest, 1, 2, 0, 1)
 x3ptest21 <- sample_x3p(x3ptest, 2, 1)
+x3ptest21.1 <- sample_x3p(x3ptest, 2, 1, 1, 0)
+
 test_that("sample_x3p works as expected", {
-
-
+  expect_equivalent(dim(x3ptest12$surface.matrix), c(6, 4))
+  expect_equivalent(dim(x3ptest12.1$surface.matrix), c(6, 3))
+  expect_equivalent(x3ptest$surface.matrix[,1], x3ptest12$surface.matrix[,1])
+  expect_equivalent(x3ptest$surface.matrix[,2], x3ptest12.1$surface.matrix[,1])
+  expect_equivalent(dim(x3ptest21$surface.matrix), c(3, 7))
+  expect_equivalent(dim(x3ptest21.1$surface.matrix), c(3, 7))
+  expect_equivalent(x3ptest$surface.matrix[1,], x3ptest21$surface.matrix[1,])
+  expect_equivalent(x3ptest$surface.matrix[2,], x3ptest21.1$surface.matrix[1,])
 })
