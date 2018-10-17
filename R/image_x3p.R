8e8d33417beabdb1a7735c71fe58cb65976fdfd6
srvander <srvanderplas@gmail.com>
2018-10-17 10:42:30 -0500

    image_x3p is now what was image_x3p_crosscut

--- R/image_x3p.R
+++ R/image_x3p.R
@@ -4,70 +4,6 @@
 #' @param file file name for saving, if file is NULL the opengl device stays open.
 #' The file extension determines the type of output. Possible extensions are png, stl (suitable for 3d printing), or svg.
 #' @param col color specification
-#' @param size vector of width and height
-#' @param zoom numeric value indicating the amount of zoom
-#' @param multiply exaggerate the relief by factor multiply
-#' @param ... not used
-#' @export
-#' @import rgl
-#' @importFrom rgl snapshot3d r3dDefaults
-#' @examples
-#' \dontrun{
-#' logo <- read_x3p(system.file("csafe-logo.x3p", package="x3ptools"))
-#' image_x3p(logo, file = "logo.png")
-#' }
-image_x3p <- function(x3p, file = NULL, col = "#cd7f32", size = c(750, 250), zoom = 0.35, multiply = 5, ...) {
-  stopifnot("x3p" %in% class(x3p))
-  surface <- x3p$surface.matrix
-  z <- multiply * surface # Exaggerate the relief
-  y <- x3p$header.info$incrementY * (ncol(z):1) #
-  x <- x3p$header.info$incrementX * (1:nrow(z)) #
-
-  params <- rgl::r3dDefaults
-  #  params$viewport <- c(0,0, 750, 250)
-  #
-  params$windowRect <- c(40, 125, 40 + size[1], 125 + size[2])
-  params$userMatrix <- diag(c(1, 1, 1, 1))
-  params$zoom <- zoom
-
-  open3d(params = params)
-  rgl.pop("lights")
-  #  xyz <- matrix(c(-2000, mean(y), max(z, na.rm=TRUE)), ncol = 3)
-  xyz <- matrix(c(
-    min(y) - diff(range(y)),
-    mean(y), max(z, na.rm = TRUE)
-  ), ncol = 3)
-
-  light3d(
-    x = xyz, diffuse = "gray40",
-    specular = "gray40", ambient = "grey10", viewpoint.rel = TRUE
-  )
-  light3d(diffuse = "gray20", specular = "gray20")
-
-  surface3d(x, y, z, color = col, back = "lines")
-
-  if (!is.null(file)) {
-    splits <- strsplit(file, split = "\\.")
-    extension <- splits[[1]][length(splits[[1]])]
-    if (extension == "png") {
-      rgl.snapshot(filename = file)
-    }
-    if (extension == "svg") {
-      rgl.postscript(filename = file, fmt = "svg")
-    }
-    if (extension == "stl") {
-      writeSTL(con = file)
-    }
-    rgl.close()
-  }
-}
-
-#' Plot x3p object as an image with crosscut marked
-#'
-#' @param x3p x3p object
-#' @param file file name for saving, if file is NULL the opengl device stays open.
-#' The file extension determines the type of output. Possible extensions are png, stl (suitable for 3d printing), or svg.
-#' @param col color specification
 #' @param crosscut crosscut index
 #' @param ccParam list with named components, consisting of parameters for showing crosscuts:  color and radius for crosscut region
 #' @param size vector of width and height
@@ -80,13 +16,13 @@ image_x3p <- function(x3p, file = NULL, col = "#cd7f32", size = c(750, 250), zoo
 #' @examples
 #' \dontrun{
 #' logo <- read_x3p(system.file("csafe-logo.x3p", package="x3ptools"))
-#' image_x3p_crosscut(logo, file = "logo.png", crosscut = 50)
+#' image_x3p(logo, file = "logo.png", crosscut = 50)
 #' }
-image_x3p_crosscut <- function(x3p, file = NULL, col = "#cd7f32",
-                               crosscut = NA, 
-                               ccParam = list(color = "#e6bf98",
-                                              radius = 5),
-                               size = c(750, 250), zoom = 0.35, multiply = 5, ...) {
+image_x3p <- function(x3p, file = NULL, col = "#cd7f32",
+                      crosscut = NA, 
+                      ccParam = list(color = "#e6bf98",
+                                     radius = 5),
+                      size = c(750, 250), zoom = 0.35, multiply = 5, ...) {
   stopifnot("x3p" %in% class(x3p))
   surface <- x3p$surface.matrix
   z <- multiply * surface # Exaggerate the relief
--- tests/testthat/test_image_x3p.R
+++ tests/testthat/test_image_x3p.R
@@ -26,19 +26,19 @@ test_that("image_x3p works as expected", {
   expect_true(file.exists("x3ptest.stl"))
 })
 
-test_that("image_x3p_crosscut works as expected", {
-  expect_error(image_x3p_crosscut("hello world"), ".x3p. .in. class.*x3p.* is not TRUE")
-  image_x3p_crosscut(x3ptest, crosscut = 3, crosscut_rad = 1)
-  rglwindowopen <- rgl::.check3d()
-  # Check that a window is open
-  expect_gte(rglwindowopen, 1)
-  # If open, close it
-  if (rglwindowopen) {
-    rgl::rgl.close()
-  }
-  image_x3p_crosscut(x3ptest, file = "x3ptest.png")
-  expect_true(file.exists("x3ptest.png"))
-  
-  image_x3p_crosscut(x3ptest, file = "x3ptest.stl")
-  expect_true(file.exists("x3ptest.stl"))
-})
+# test_that("image_x3p_crosscut works as expected", {
+#   expect_error(image_x3p_crosscut("hello world"), ".x3p. .in. class.*x3p.* is not TRUE")
+#   image_x3p_crosscut(x3ptest, crosscut = 3, crosscut_rad = 1)
+#   rglwindowopen <- rgl::.check3d()
+#   # Check that a window is open
+#   expect_gte(rglwindowopen, 1)
+#   # If open, close it
+#   if (rglwindowopen) {
+#     rgl::rgl.close()
+#   }
+#   image_x3p_crosscut(x3ptest, file = "x3ptest.png")
+#   expect_true(file.exists("x3ptest.png"))
+#   
+#   image_x3p_crosscut(x3ptest, file = "x3ptest.stl")
+#   expect_true(file.exists("x3ptest.stl"))
+# })
