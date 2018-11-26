49dadb267489d996934380626add636e3fb038cc
Susan Vanderplas <srvanderplas@gmail.com>
2018-11-26 15:57:40 -0600

    test convert.R

---
+++ tests/testthat/test_convert.R
@@ -0,0 +1,7 @@
+context("convert")
+
+
+test_that("convert works as expected", {
+  x3ptest_mum <- x3p_m_to_mum(x3ptest)
+  expect_equivalent(x3ptest$surface.matrix, x3ptest_mum$surface.matrix/10^6)
+})
