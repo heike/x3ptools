49139090fe1ed43e72cdc170627a090e694abf6b
heike <hofmann@iastate.edu>
2019-02-28 08:12:33 -0600

    showing and modifying individual elements of the meta file of x3p objects

---
+++ R/modify_xml.R
@@ -0,0 +1,64 @@
+#' Modify xml elements meta information in x3p object
+#' 
+#' Identify xml fields in the meta file of an x3p object by name and modify content if uniquely described.
+#' @param x3p x3p object
+#' @param element character. Name of xml field in the meta file.
+#' @param value character. Value to be given to the xml field in the meta file.
+#' @return x3p object with changed meta information
+#' @export
+#' @examples 
+#' logo <- read_x3p(system.file("csafe-logo.x3p", package="x3ptools"))
+#' x3p_show_xml(logo, "creator")
+#' x3p_modify_xml(logo, "creator", "I did that") 
+x3p_modify_xml <- function(x3p, element, value) {
+  find_element <- helper_identify_xml(x3p, element)
+  idx <- find_element[[1]]
+  res <- find_element[[2]]
+
+  if (length(res) == 0) stop(sprintf("No element found matching <%s>", element))
+  if (length(res) > 1) stop(sprintf("More than one element matching <%s> found: %s", element, paste(names(res), collapse=", ")))
+  
+  # we found exactly one matching meta element
+  record <- NULL
+  n = find_element[[3]]
+  record <- which(idx <= cumsum(n))[1]
+
+  sm <- which(names(x3p)=="surface.matrix")
+  obj_str <- sprintf("x3p[-sm][[record]]$%s[[1]] <- value", gsub("\\.", "$", names(res)))
+  
+  eval(parse(text=obj_str))
+  x3p
+}
+
+
+helper_identify_xml <- function(x3p, element) {
+  # there are four records to look for names
+  rec1 <- x3p$header.info
+  rec2 <- unlist(x3p$feature.info)
+  rec3 <- unlist(x3p$general.info)
+  rec4 <- unlist(x3p$matrix.info)
+  allrecords <- c(rec1,rec2,rec3,rec4)
+
+  idx <- grep(tolower(element), tolower(names(allrecords)))
+  
+  n = c(length(rec1), length(rec2), length(rec3), length(rec4))
+  
+  list(idx, allrecords[idx], n)
+}
+
+#' Show xml elements from meta information in x3p object
+#' 
+#' Identify xml fields by name and show content
+#' @param x3p x3p object
+#' @param element character. Name of xml field in the meta file.
+#' @return list of exact field names and their contents
+#' @export
+#' @examples 
+#' logo <- read_x3p(system.file("csafe-logo.x3p", package="x3ptools"))
+#' x3p_show_xml(logo, "creator")
+#' x3p_show_xml(logo, "axis")
+#' x3p_show_xml(logo, "CZ.AxisType")
+x3p_show_xml <- function(x3p, element) {
+  res <- helper_identify_xml(x3p, element)
+  res[[2]]
+}
