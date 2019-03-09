d176fb86ae3115e83f7879b0d562617a37b35843
Susan Vanderplas <srvanderplas@gmail.com>
2019-03-09 13:56:56 -0600

    test image_x3p with gridlines

--- tests/testthat/test_image_x3p.R
+++ tests/testthat/test_image_x3p.R
@@ -31,7 +31,8 @@ test_that("image_x3p works as expected", {
   if (rglwindowopen) {
     rgl::rgl.close()
   }
-  image_x3p(x3ptest, file = "x3ptest.png")
+  x3ptest2 <- x3ptest %>% x3p_add_grid(spaces = 2, size = 1, color = "black")
+  image_x3p(x3ptest2, file = "x3ptest.png")
   expect_true(file.exists("x3ptest.png"))
   image_x3p(x3ptest, file = "x3ptest.svg")
   expect_true(file.exists("x3ptest.svg"))
@@ -59,38 +60,3 @@ test_that("image_x3p works as expected", {
     rgl::rgl.close()
   }
 })
-
-# test_that("image_x3p_grid works as expected", {
-#   expect_error(image_x3p_grid("hello world"), ".x3p. .in. class.*x3p.* is not TRUE")
-#   image_x3p_grid(x3ptest)
-#   rglwindowopen <- rgl::.check3d()
-#   # Check that a window is open
-#   expect_gte(rglwindowopen, 1)
-#   # If open, close it
-#   if (rglwindowopen) {
-#     rgl::rgl.close()
-#   }
-#   image_x3p_grid(x3ptest, file = "x3ptest2.png")
-#   expect_true(file.exists("x3ptest2.png"))
-#   image_x3p_grid(x3ptest, file = "x3ptest2.svg")
-#   expect_true(file.exists("x3ptest2.svg"))
-#   image_x3p_grid(x3ptest, file = "x3ptest2.stl")
-#   expect_true(file.exists("x3ptest2.stl"))
-# })
-
-# test_that("image_x3p_crosscut works as expected", {
-#   expect_error(image_x3p_crosscut("hello world"), ".x3p. .in. class.*x3p.* is not TRUE")
-#   image_x3p_crosscut(x3ptest, crosscut = 3, crosscut_rad = 1)
-#   rglwindowopen <- rgl::.check3d()
-#   # Check that a window is open
-#   expect_gte(rglwindowopen, 1)
-#   # If open, close it
-#   if (rglwindowopen) {
-#     rgl::rgl.close()
-#   }
-#   image_x3p_crosscut(x3ptest, file = "x3ptest.png")
-#   expect_true(file.exists("x3ptest.png"))
-#   
-#   image_x3p_crosscut(x3ptest, file = "x3ptest.stl")
-#   expect_true(file.exists("x3ptest.stl"))
-# })
