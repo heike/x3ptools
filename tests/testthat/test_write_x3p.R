ed5738e0a60d86855cf58d215bbf9e72c161c3da
srvander <srvanderplas@gmail.com>
2019-03-11 08:37:10 -0500

    make write_x3p test quiet

--- tests/testthat/test_write_x3p.R
+++ tests/testthat/test_write_x3p.R
@@ -4,7 +4,7 @@ context("write_x3p")
 test_that("write_x3p works as expected", {
   tmpfile <- tempfile(fileext = ".x3p")
   # write a copy of the file into a temporary file
-  write_x3p(x3ptest, file = tmpfile)
+  write_x3p(x3ptest, file = tmpfile, quiet = T)
   tmpx3p <- read_x3p(tmpfile)
   expect_equivalent(tmpx3p$surface.matrix, x3ptest$surface.matrix)
   expect_equivalent(tmpx3p$header.info$sizeX, x3ptest$header.info$sizeX)
