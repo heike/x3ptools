7741bcae835986a1a66b42805e0f7f55cee5ffe4
Susan Vanderplas <srvanderplas@gmail.com>
2019-03-20 18:19:45 -0500

    text sample_x3p with mask

--- tests/testthat/test_sample_x3p.R
+++ tests/testthat/test_sample_x3p.R
@@ -14,4 +14,6 @@ test_that("sample_x3p works as expected", {
   expect_equivalent(dim(x3ptest21.1$surface.matrix), c(3, 7))
   expect_equivalent(x3ptest$surface.matrix[1,], x3ptest21$surface.matrix[1,])
   expect_equivalent(x3ptest$surface.matrix[2,], x3ptest21.1$surface.matrix[1,])
+  
+  expect_silent(sample_x3p(x3pbig, 1, 2))
 })
