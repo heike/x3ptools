c430e4b99151cb325f840853059b53fac928ff1e
Susan Vanderplas <srvanderplas@gmail.com>
2019-03-20 18:21:19 -0500

    better test for sample_x3p

--- tests/testthat/test_sample_x3p.R
+++ tests/testthat/test_sample_x3p.R
@@ -5,6 +5,8 @@ x3ptest12.1 <- sample_x3p(x3ptest, 1, 2, 0, 1)
 x3ptest21 <- sample_x3p(x3ptest, 2, 1)
 x3ptest21.1 <- sample_x3p(x3ptest, 2, 1, 1, 0)
 
+x3pbig.sample <- sample_x3p(x3pbig, 1, 2)
+
 test_that("sample_x3p works as expected", {
   expect_equivalent(dim(x3ptest12$surface.matrix), c(6, 4))
   expect_equivalent(dim(x3ptest12.1$surface.matrix), c(6, 3))
@@ -15,5 +17,5 @@ test_that("sample_x3p works as expected", {
   expect_equivalent(x3ptest$surface.matrix[1,], x3ptest21$surface.matrix[1,])
   expect_equivalent(x3ptest$surface.matrix[2,], x3ptest21.1$surface.matrix[1,])
   
-  expect_silent(sample_x3p(x3pbig, 1, 2))
+  expect_equivalent(dim(x3pbig.sample$surface.matrix), dim(x3pbig.sample$mask))
 })
