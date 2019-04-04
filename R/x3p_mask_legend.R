2cd852ac1cd24465b339191e42c1d7d72cc82f29
heike <hofmann@iastate.edu>
2019-04-04 17:41:03 -0500

    and the background now actually shows up in the legend ...

--- R/x3p_mask_legend.R
+++ R/x3p_mask_legend.R
@@ -18,7 +18,7 @@ x3p_mask_legend <- function(x3p) {
   
   background <- x3p_show_xml(x3p, "Background")
   if (length(background) > 0) {
-    colors <- c(background[1], colors)
+    colors <- c(background[[1]], colors)
     names(colors)[1] <- "background"
   }
   colors
