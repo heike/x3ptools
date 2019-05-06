6c6163519d570818eeb5748174b9445c295a55c0
Susan Vanderplas <srvanderplas@gmail.com>
2019-05-06 15:20:08 -0500

    tests for x3p_mask_quantile

---
+++ tests/testthat/test_x3p_mask_quantile.R
@@ -0,0 +1,25 @@
+context("x3p_mask_quantile")
+
+test_that("x3p_mask_quantile works", {
+  expect_error(x3p_mask_quantile("helloworld"))
+  expect_equivalent(
+    x3p_mask_quantile(x3ptest, quantiles = c(.5, 1))$mask,
+    structure(c("red", "red", "red", "red", "red", "red", "red", 
+                "red", "red", "red", "red", "red", "red", "red", "red", "#cd7f32", 
+                "#cd7f32", "#cd7f32", "#cd7f32", "#cd7f32", "#cd7f32", "#cd7f32", 
+                "#cd7f32", "#cd7f32", "#cd7f32", "#cd7f32", "#cd7f32", "#cd7f32", 
+                "#cd7f32", "#cd7f32", "#cd7f32", "#cd7f32", "#cd7f32", "red", 
+                "red", "red", "red", "red", "red", "red", "red", "red"), 
+              .Dim = 7:6, class = "raster")
+  )
+  expect_equivalent(
+    x3p_mask_quantile(x3ptest, quantiles = .2)$mask,
+    structure(c("#cd7f32", "#cd7f32", "#cd7f32", "#cd7f32", "#cd7f32", 
+                "#cd7f32", "red", "#cd7f32", "red", "red", "red", "red", "red", 
+                "red", "red", "red", "red", "red", "#cd7f32", "#cd7f32", "#cd7f32", 
+                "#cd7f32", "#cd7f32", "#cd7f32", "#cd7f32", "#cd7f32", "#cd7f32", 
+                "#cd7f32", "#cd7f32", "#cd7f32", "red", "red", "red", "red", 
+                "red", "red", "#cd7f32", "red", "#cd7f32", "#cd7f32", "#cd7f32", 
+                "#cd7f32"), .Dim = 7:6, class = "raster")
+  )
+})
