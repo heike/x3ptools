af795069dd3cd86f1e8c3f49194fe5aa91681f36
heike <hofmann@iastate.edu>
2021-03-25 01:06:24 -0500

    include fuzzy selection

--- R/image_x3p.R
+++ R/image_x3p.R
@@ -9,6 +9,7 @@
 #' @param size vector of width and height
 #' @param zoom numeric value indicating the amount of zoom
 #' @param multiply exaggerate the relief by factor multiply
+#' @param update Boolean value indicating whether a scene should be updated (defaults to FALSE). If FALSE, a new rgl device is opened.
 #' @param ... not used
 #' @export
 #' @import rgl
@@ -27,7 +28,7 @@ x3p_image <- function(x3p, file = NULL, col = "#cd7f32",
                         color = "#e6bf98",
                         radius = 5
                       ),
-                      size = c(750, 250), zoom = 0.35, multiply = 5, ...) {
+                      size = c(750, 250), zoom = 0.35, multiply = 5, update = FALSE, ...) {
   stopifnot("x3p" %in% class(x3p))
   surface <- x3p$surface.matrix
   z <- multiply * surface # Exaggerate the relief
@@ -43,19 +44,26 @@ x3p_image <- function(x3p, file = NULL, col = "#cd7f32",
   params$userMatrix <- diag(c(1, 1, 1, 1))
   params$zoom <- zoom
 
-  open3d(params = params)
-  rgl.pop("lights")
+  
   #  xyz <- matrix(c(-2000, mean(y), max(z, na.rm=TRUE)), ncol = 3)
   xyz <- matrix(c(
     min(y) - diff(range(y)),
     mean(y), max(z, na.rm = TRUE)
   ), ncol = 3)
+  
+  if (!update) { 
+    open3d(params = params)
+    rgl.pop("lights")
+    light3d(
+      x = xyz, diffuse = "gray40",
+      specular = "gray40", ambient = "grey10", viewpoint.rel = TRUE
+    )
+    light3d(diffuse = "gray20", specular = "gray20")
+  } else {
+#    pop3d()
+  }
+  
 
-  light3d(
-    x = xyz, diffuse = "gray40",
-    specular = "gray40", ambient = "grey10", viewpoint.rel = TRUE
-  )
-  light3d(diffuse = "gray20", specular = "gray20")
 
   if (!is.na(crosscut)) {
     .Deprecated("x3p_add_hline", msg = "Use of crosscut is deprecated. Use x3p_add_hline instead.")
--- R/x3p_select.R
+++ R/x3p_select.R
@@ -23,8 +23,16 @@
 #'   x3p_select(logo, update=TRUE, col="#00FF00") 
 #' }
 x3p_select <- function(x3p, col = "#FF0000", update=TRUE) {
-  #  browser()
+ #   browser()
   stopifnot("x3p" %in% class(x3p))
+  ids <- rgl::ids3d()
+  if (nrow(ids) == 0) {
+    size <- dim(x3p$surface.matrix)
+    size[2] <- round(500/size[1]*size[2])
+    size[1] <- 500
+    x3p %>% x3p_image(size = size, zoom=0.8)
+  }
+  rgl::rgl.bringtotop()
   f <- select3d()
   
   multiply <- 5
@@ -38,10 +46,111 @@ x3p_select <- function(x3p, col = "#FF0000", update=TRUE) {
   
   if (is.null(x3p$mask)) x3p <- x3p %>% x3p_add_mask()
   #  
+  
   x3p$mask[matrix(selected, nrow = dim(x3p$mask)[1], byrow=TRUE)] <- col
   if (update) {
+ #   browser()
     pop3d() # remove the active scene
     surface3d(x, y, z, as.vector(x3p$mask), back = "fill")
+  #  x3p %>% image_x3p(update=TRUE)
   }
+  x3p
+}
+
+
+#' Interactive selection of region of interest
+#' 
+#' 
+#' @param x3p x3p file
+#' @param col character value of the selection color
+#' @param mad scalar
+#' @param type only "plane" is implemented at the moment
+#' @param update boolean value, whether the rgl window should be updated to show the selected rectangle
+#' @return x3p file with updated mask
+#' @importFrom stats predict
+#' @importFrom MASS rlm
+#' @export
+#' @examples 
+#' \dontrun{
+#' if (interactive) {
+#'   if (!file.exists("fadul1-1.x3p")) {
+#'     url <- "https://tsapps.nist.gov/NRBTD/Studies/CartridgeMeasurement/DownloadMeasurement"
+#'     file <- "2d9cc51f-6f66-40a0-973a-a9292dbee36d"
+#'     download.file(file.path(url, file), destfile="fadul1-1.x3p")
+#'   }
+#'   x3p <- x3p_read("fadul1-1.x3p")
+#'   x3p_image(x3p, size=c(500,500), zoom=.8)
+#'   x3p <- x3p_fuzzyselect(x3p, update=TRUE, col="#FF0000") 
+#' 
+#'   logo <- x3p_read(system.file("csafe-logo.x3p", package="x3ptools"))
+#'   x3p_image(logo, size=c(500,500), zoom = 1)
+#'   x3p_fuzzyselect(logo, update=TRUE, col="#00FF00") 
+#' }
+x3p_fuzzyselect <- function(x3p, col="#FF0000", mad=5, type="plane", update=TRUE) {
+  ids <- rgl::ids3d()
+  if (nrow(ids) == 0) {
+    size <- dim(x3p$surface.matrix)
+    size[2] <- round(500/size[1]*size[2])
+    size[1] <- 500
+    x3p_image(x3p, size = size, zoom=0.8)
+  }
+  rgl::rgl.bringtotop()
+  
+  mask <- NULL 
+
+  stop <- FALSE
+  cat("select rectangular area or click to stop\n")
+  
+  while(!stop) {
+    tab1 <- table(as.vector(x3p$mask))
+    x3p <- x3p_select(x3p, col=col)
+    
+    tab2 <- table(as.vector(x3p$mask))
+    stop <- identical(tab1, tab2)
+    
+    if (stop) {
+      cat("exiting selection.")
+      
+      return(x3p)
+    }
+    
+    x3p_df <- x3p_to_df(x3p)
+    # x3p_df %>% count(mask)
+    
+    if (type== "plane") {
+      filtered_region <- dplyr::filter(x3p_df, mask == col)
+      m1 <-   MASS::rlm(value ~ x + y, data = filtered_region)
+      
+      
+      cat("... adding similar cases \n")
+      
+      x3p_df$p1 <- predict(m1, newdata = x3p_df)
+      x3p_df$r1 <- abs(x3p_df$value - x3p_df$p1) 
+      
+      #x3p_df <- x3p_df %>% filter(r1 < 1*max(abs(m1$residuals)))
+      
+      x3p_df$mask[which(x3p_df$r1 < mad*max(.Machine$double.eps, mad(m1$residuals)))] = col
+    }
+    if (type == "distance") {
+      # idea: color in all pixels 
+    }
+    # convert back to x3p
+    x3p <- df_to_x3p(x3p_df)
+    
+    if (update) {
+      rgl::pop3d()
+      #   browser()
+      surface <- x3p$surface.matrix
+      z <- 5 * surface
+      yidx <- ncol(z):1
+      y <- x3p$header.info$incrementY * yidx
+      x <- x3p$header.info$incrementX * (1:nrow(x3p$surface.matrix))
+      rgl::surface3d(x, y, z, x3p_df$mask, back = "fill")
+    }
+    cat("select a rectangular area on the active rgl device or click on white space to stop ...\n")
+    
+  }
+  
+  cat("exiting selection.")
   x3p
 }
