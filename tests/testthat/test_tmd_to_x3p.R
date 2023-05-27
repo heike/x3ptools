c86ee2a34f53738c654b027ae307671114455234
Heike Hofmann <hofmann@iastate.edu>
2023-05-26 23:22:35 -0500

    couple more lines covered

--- tests/testthat/test_tmd_to_x3p.R
+++ tests/testthat/test_tmd_to_x3p.R
@@ -10,5 +10,10 @@ test_that("tmd_to_x3p works as expected", {
   x3p <- x3p_read(system.file("testing.x3p", package="x3ptools"))
   expect_identical(tmd$surface.matrix, x3p$surface.matrix)
 
+  tmd_file_2 <- system.file("gelsight/testing.tmd", package="x3ptools")
+  
+  expect_output(tmd2 <- tmd_to_x3p(tmd_file_2), regex="Reading meta information from file scan.yaml.") 
+  expect_identical(tmd$surface.matrix, tmd2$surface.matrix)
+  expect_match(x3p_show_xml(tmd2, "Instrument.Model")$Instrument.Model ,"GelSight Mobile 1X")
 })
 
