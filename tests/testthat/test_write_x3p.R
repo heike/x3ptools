fb64d0be30f17509508f8cc6b28ca40e3d2c4651
Heike Hofmann <hofmann@iastate.edu>
2023-05-30 14:12:58 -0500

    chatty version

--- tests/testthat/test_write_x3p.R
+++ tests/testthat/test_write_x3p.R
@@ -5,6 +5,11 @@ test_that("write_x3p works as expected", {
   tmpfile <- tempfile(fileext = ".x3p")
   # write a copy of the file into a temporary file
   write_x3p(x3ptest, file = tmpfile, quiet = T)
+  # make it chatty
+  expect_message(write_x3p(x3ptest, file = tmpfile, quiet = F),
+                regexp="general info not specified, using template")
+  
+  
   tmpx3p <- read_x3p(tmpfile)
   expect_equivalent(tmpx3p$surface.matrix, x3ptest$surface.matrix)
   expect_equivalent(tmpx3p$header.info$sizeX, x3ptest$header.info$sizeX)
