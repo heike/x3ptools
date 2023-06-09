2afc256f21b0e6525b3b31b3f0d469e4e61ddef1
Heike Hofmann <hofmann@iastate.edu>
2023-06-08 22:35:01 -0500

    profile segments

--- R/x3p_extract_profile_segments.R
+++ R/x3p_extract_profile_segments.R
@@ -1,19 +1,23 @@
-#' Extract profiles with multiple segments
+#' Extract profiles from surface using multiple segments
 #' 
+#' The 3d topographic surface is split into multiple segments of width `width` (in pixels)
+#' using an overlap of 10% between segments. For each segment, a line is extracted (with `x3p_extract_profile`).
+#' Line segments are projected onto the mask of the initial x3p object and exported as a `lines` attribute. 
 #' @param x3p object
 #' @param width segment width 
 #' @param col color
 #' @param linewidth integer value specifying the width for the profile
 #' @param verbose logical 
-#' @return x3p object with added lines
+#' @return x3p object with added `lines` attribute.
 #' @importFrom dplyr mutate left_join select desc add_tally ungroup
 #' @importFrom purrr pmap map map_dbl pmap_df 
+#' @export
 #' @examples 
 #' logo <- x3p_read(system.file("csafe-logo.x3p", package="x3ptools"))
 #' logo <- x3p_m_to_mum(logo)
 #' if(interactive())
-#'   x3p_extract_profile_segments(logo, 850, col="#ffffff")
-x3p_extract_profile_segments <- function(x3p, width, col, linewidth, verbose = TRUE) {
+#'   x3p_extract_profile_segments(logo, 850, col="#ffffff", linewidth=5)
+x3p_extract_profile_segments <- function(x3p, width, col="#00FF00", linewidth, verbose = TRUE) {
   # pass R CMD CHECK
   x <- y <- height <- value <- orig_x <- orig_y <- piece <- NULL
   mask.x <- mask.y <- NULL
@@ -46,50 +50,73 @@ x3p_extract_profile_segments <- function(x3p, width, col, linewidth, verbose = T
   dframe <- dframe %>% mutate(
     x3p = x3p %>% purrr::map(.f = function(x) {
       x %>% x3ptools::x3p_image()
-      x <- x %>% x3p_extract_profile(linewidth=5)
+      x <- x %>% x3p_extract_profile(linewidth=linewidth)
     })
   )
-  if (verbose) {
-    message("done\nCombine profiles into one ...\n")
-  }
-  dframe <- dframe %>% mutate(
-    first10 = x3p %>% purrr::map_dbl(.f = function(x) {
-      x$line_df %>% filter(x <= w10) %>% summarize(value = mean(value, na.rm=TRUE)) %>% pull(1)
-    }),
-    last10 = x3p %>% purrr::map_dbl(.f = function(x) {
-      x$line_df %>% filter(x > w90) %>% summarize(value = mean(value, na.rm=TRUE)) %>% pull(1)
-    })
-  )
-  idx <-1:nrow(dframe)
-  dframe$y_adjust <- 0
-  if (length(idx) > 1) {
-  idx_last <- c(NA, 1:(nrow(dframe)-1))
-  
-  dframe$y_adjust <- dframe$last10[idx_last]-dframe$first10[idx]
-  dframe$y_adjust[1] <- 0
-  dframe$y_adjust <- cumsum(dframe$y_adjust)
-  
-  }
-  
-  lines <- purrr::pmap_df(list(dframe$x3p, dframe$x, dframe$y_adjust),
-                          .f = function(x3p, offset, adjust) {
-                            x3p$line_df %>% mutate(orig_x = orig_x + offset, value = value+adjust)
-                          }, .id="piece")
-  
-  lines$mask <- col
-  if (is.null(x3p$mask)) x3p <- x3p %>% x3p_add_mask()
-  x3p_df <- x3p %>% x3p_to_df()
-  lines <- lines %>% dplyr::select(-x) %>% 
-    rename(y = orig_y, x = orig_x)
+ 
   if (verbose) {
     message("done\nIncorporate into mask ...\n")
   }
   
-  x3p_df <- x3p_df %>% left_join(
-    lines %>% dplyr::select(-value, -piece) %>% unique(), by=c("x", "y"))
+  dframe <- dframe %>% mutate(
+    line = x3p %>% purrr::map(.f = function(x) x$line)
+  )
+  
+  masklines <- dframe %>% select(-x3p) %>% 
+    rename(offset_x = x, offset_y = y) %>%
+    mutate(piece = 1:n()) %>%
+    tidyr::unnest(col=line)
+  
+  masklines$mask <- col
+  masklines <- masklines %>% mutate(
+    x = round(orig_x + offset_x),
+    y = round(orig_y + offset_y)
+  ) %>% select(x, y, mask) %>% 
+    unique()
+
+  if (is.null(x3p$mask)) x3p <- x3p %>% x3p_add_mask()
+  
+  x3p_df <- x3p %>% x3p_to_df()
+ # masklines %>% anti_join(x3p_df, by=c("x", "y")) 
+  
+  x3p_df <- x3p_df %>% left_join(masklines, by=c("x", "y"))
   x3p_df <- x3p_df %>% mutate(
     mask = ifelse(is.na(mask.y), mask.x, mask.y)
-  ) %>% dplyr::select(-mask.x, -mask.y)
+  ) %>% select(-mask.x, -mask.y)
+  
+  if (verbose) {
+    message("done\nCombine profiles into one ...\n")
+  }
+
+  # check the `value` values of overlapping pieces and adjust consecutive pieces for any systematic 
+  # differences in `value` 
+   dframe <- dframe %>% mutate(
+     value_first10 = x3p %>% purrr::map_dbl(.f = function(x) {
+       x$line %>% filter(x <= w10) %>% summarize(value = mean(value, na.rm=TRUE)) %>% pull(1)
+     }),
+     value_last10 = x3p %>% purrr::map_dbl(.f = function(x) {
+       x$line %>% filter(x > w90) %>% summarize(value = mean(value, na.rm=TRUE)) %>% pull(1)
+     })
+   )
+   idx <-1:nrow(dframe)
+   dframe$value_adjust <- 0
+   if (length(idx) > 1) {
+     idx_last <- c(NA, 1:(nrow(dframe)-1))
+  
+     dframe$value_adjust <- dframe$value_last10[idx_last]-dframe$value_first10[idx]
+     dframe$value_adjust[1] <- 0
+     dframe$value_adjust <- cumsum(dframe$value_adjust)
+   }
+   lines <- dframe %>% select(-x3p) %>% 
+     rename(offset_x = x, offset_y = y) %>%
+     mutate(piece = 1:n()) %>%
+     tidyr::unnest(col=line) %>%
+     mutate(
+       value = value + value_adjust,
+       x = orig_x+offset_x
+       )
+   # lines %>% ggplot(aes(x = orig_x+offset_x, y = value)) + geom_point(aes(colour=factor(piece)))
+   
   
   if (verbose) {
     message("done")
@@ -100,6 +127,12 @@ x3p_extract_profile_segments <- function(x3p, width, col, linewidth, verbose = T
   x3p$mask <- as.raster(mask)
   x3p$header.info$incrementY <- orig_scale
   x3p$header.info$incrementX <- orig_scale
-  x3p$line_df <- lines %>% mutate(x = x * orig_scale, y = y * orig_scale)
+  
+  x3p$lines <- lines %>% mutate(
+    x = x * orig_scale, 
+    y = y * orig_scale,
+    orig_x = orig_x+offset_x,
+    orig_y = orig_y+offset_y) %>%
+    select(x, y, value, orig_x, orig_y, piece)
   x3p
 }
