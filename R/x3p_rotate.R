dc2f3fbf96d318dcfb4d4ff81762c7081e4d3345
Yuhang (Tom) Lin <yhlin@iastate.edu>
2024-02-21 14:52:48 -0600

    Solve for the boundary

--- R/x3p_rotate.R
+++ R/x3p_rotate.R
@@ -3,7 +3,7 @@
 #' Rotate the surface matrix and mask of an x3p object. Also adjust meta information.
 #' @param x3p x3p object
 #' @param angle rotate counter-clockwise by angle in degrees.
-#' @importFrom imager as.cimg pad imrotate
+#' @importFrom imager cimg as.cimg pad imrotate
 #' @importFrom dplyr near
 #' @export
 #' @examples
@@ -23,46 +23,50 @@
 x3p_rotate <- function(x3p, angle = 90) {
   stopifnot(is.numeric(angle))
   angle <- angle %% 360
-  theta <- angle / 180 * pi
   if (near(angle, 0)) {
     return(x3p)
   }
-  x3p_shift <- x3p$surface.matrix
-  NA_val <- min(x3p$surface.matrix, na.rm = TRUE) - 0.1 *
-    diff(range(x3p$surface.matrix, na.rm = TRUE))
+  not_zero <- 10
+  shift_up <- not_zero + abs(min(x3p$surface.matrix, na.rm = TRUE))
+  x3p_shift <- x3p$surface.matrix + shift_up 
+  NA_val <- 0
+  
   x3p_shift[is.na(x3p$surface.matrix)] <- NA_val
   x3p_cimg <- as.cimg(x3p_shift)
-  x3p_cimg_pad_rotate <- x3p_cimg %>% imrotate(-angle)
-  x3p_matrix_pad_rotate <- x3p_cimg_pad_rotate %>% as.matrix()
-  x3p_matrix_pad_rotate[near(x3p_matrix_pad_rotate, NA_val)] <- NA
-  na_matrix <- x3p_matrix_pad_rotate %>% is.na()
+
+  x3p_cimg_rotate <- x3p_cimg %>%
+    imrotate(-angle, interpolation = 0L)
+  x3p_matrix_rotate <- x3p_cimg_rotate %>%
+    as.matrix()
+  x3p_matrix_rotate[x3p_matrix_rotate < shift_up/2] <- NA # there should not be any values between 0 and shift_up
+  x3p_matrix_rotate <- x3p_matrix_rotate - shift_up - not_zero
+  
+  na_matrix <- x3p_matrix_rotate %>% is.na()
   na_row <- rowSums(na_matrix) == (ncol(na_matrix))
   na_col <- colSums(na_matrix) == (nrow(na_matrix))
-  x3p_matrix_pad_rotate <- x3p_matrix_pad_rotate[
+  x3p_matrix_rotate <- x3p_matrix_rotate[
     !na_row,
     !na_col
   ]
-  x3p_pad_rotate <- x3p
-  x3p_pad_rotate$header.info$sizeX <- nrow(x3p_matrix_pad_rotate)
-  x3p_pad_rotate$header.info$sizeY <- ncol(x3p_matrix_pad_rotate)
-  x3p_pad_rotate$matrix.info$MatrixDimension$SizeX <- list(nrow(x3p_matrix_pad_rotate))
-  x3p_pad_rotate$matrix.info$MatrixDimension$SizeY <- list(ncol(x3p_matrix_pad_rotate))
-  x3p_pad_rotate$surface.matrix <- x3p_matrix_pad_rotate
+  x3p_rotate <- x3p
+  x3p_rotate$header.info$sizeX <- nrow(x3p_matrix_rotate)
+  x3p_rotate$header.info$sizeY <- ncol(x3p_matrix_rotate)
+  x3p_rotate$matrix.info$MatrixDimension$SizeX <- list(nrow(x3p_matrix_rotate))
+  x3p_rotate$matrix.info$MatrixDimension$SizeY <- list(ncol(x3p_matrix_rotate))
+  x3p_rotate$surface.matrix <- x3p_matrix_rotate
   if (!is.null(x3p$mask)) {
     x3p_mask_cimg <- as.cimg(x3p$mask)
-    NA_val <- "black"
-    x3p_mask_cimg_pad_rotate <- x3p_mask_cimg %>% imrotate(-angle)
-    x3p_mask_raster_pad_rotate <- x3p_mask_cimg_pad_rotate %>%
+    x3p_mask_cimg_rotate <- x3p_mask_cimg %>% imrotate(-angle, interpolation = 0L)
+    x3p_mask_raster_rotate <- x3p_mask_cimg_rotate %>%
       as.raster()
-    x3p_mask_raster_pad_rotate[x3p_mask_raster_pad_rotate ==
-      NA_val] <- NA
-    x3p_mask_raster_pad_rotate <- x3p_mask_raster_pad_rotate[
+    x3p_mask_raster_rotate[is.na(x3p_matrix_rotate)] <- NA
+    x3p_mask_raster_rotate <- x3p_mask_raster_rotate[
       !na_col,
       !na_row
     ] %>% toupper()
-    x3p_pad_rotate$mask <- x3p_mask_raster_pad_rotate
+    x3p_rotate$mask <- x3p_mask_raster_rotate
   }
-  return(x3p_pad_rotate)
+  return(x3p_rotate)
 }
 
 
