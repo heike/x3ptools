0b133b5dfe98470a75d7d51f6528c14a0e1c1a51
Heike Hofmann <hofmann@iastate.edu>
2024-05-10 08:57:47 -0500

    expand crop element

--- R/x3p_crop.R
+++ R/x3p_crop.R
@@ -8,6 +8,7 @@
 #' @param y integer, location (in pixels) of the leftmost side of the rectangle,
 #' @param width integer, width (in pixels) of the rectangle,
 #' @param height integer, height (in pixels) of the rectangle,
+#' @return x3p object of the cropped location expanded by list element `offset` that contains the coordinates of the cropped piece within the parent.
 #' @export
 #' @examples
 #' logo <- x3p_read(system.file("csafe-logo.x3p", package="x3ptools"))
@@ -27,6 +28,8 @@ x3p_crop <- function(x3p, x=1, y=1, width=128, height=128) {
   # warn in case dimensions are not what was specified by user:
   if ((length(xidx) != width) | (length(yidx) != height)) {
     warning(sprintf("Specified dimension are adjusted to fit within range of the surface matrix: %d x %d", length(xidx), length(yidx)))
+    height <- length(yidx)
+    width <-  length(xidx)          
   }
   stopifnot(length(xidx) > 0, length(yidx) > 0) # there should be something left over after adjusting
   
@@ -36,6 +39,27 @@ x3p_crop <- function(x3p, x=1, y=1, width=128, height=128) {
   y3p$matrix.info$MatrixDimension$SizeX[[1]] <- y3p$header.info$sizeX
   y3p$matrix.info$MatrixDimension$SizeY[[1]] <- y3p$header.info$sizeY
   
+  # identify location of the cropped piece within (original) parent
+  xmin_prev <- 0
+  ymin_prev <- 0
+  if (!is.null(x3p$offset)) {
+    prev_offset <- x3p$offset
+    xmin_prev <- prev_offset[1,1]-1
+    ymin_prev <- prev_offset[1,2]-1
+  }
+  dims <- c(xmin = xmin_prev + x, 
+            ymin = ymin_prev + y,
+            xmax = xmin_prev+x+width-1,
+            ymax = ymin_prev+y+height-1)
+  # scale indices to current scale. 
+  # HH: I believe that we need to make sure to rescale or toss the offset info when we sample or average
+  scale <- y3p %>% x3p_get_scale()
+  scale_idx <- function(index) {scale*(index-1)}
+  
+  scaled_dims <- scale_idx(dims)
+  offset <- rbind(dims, scaled_dims)
+  offset <- data.frame(offset, units = c("index", "scaled"))
+  y3p$offset <- offset
   
   y3p <- y3p %>% x3p_paste_comment(paste0("cropped from location (", x, ",", y,")"), sep="; ")
   
